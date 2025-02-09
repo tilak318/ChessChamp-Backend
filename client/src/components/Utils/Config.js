52a18af3389d1a282cecc991d117798df71e837c
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-02-09 10:45:19 +0530

    Fix: Installed missing Babel plugin for deployment

--- client/src/components/Utils/Config.js
+++ client/src/components/Utils/Config.js
@@ -2,6 +2,7 @@ let URL = '';
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     URL = 'https://chesschamp-backend.onrender.com';
+    // URL = 'http://localhost:5000';
 } else {
     URL = '';
 }
