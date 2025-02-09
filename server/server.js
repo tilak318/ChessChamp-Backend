d00bd1eb3ef51c6f60d6a0bcae5cf23ada07555d
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-02-09 09:40:10 +0530

    Fix: Added missing Babel plugin for deployment issue

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = socketio(server, {
     cors: {
-        origin: 'https://client-az3nnrb2a-tilak-viradiyias-projects.vercel.app/',
+        origin: 'http://localhost:3000',
         credentials: true,
     },
 });
