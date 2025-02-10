21b14c1d19e62a2dbf59c76b239c60076dada1c4
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-02-10 11:11:06 +0530

    Update message

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 let SERVER_URL, CLIENT_URL;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    SERVER_URL = 'http://localhost:5000';
-    CLIENT_URL = 'http://localhost:3000';
+    SERVER_URL = 'https://chesschamp-backend.onrender.com';
+    CLIENT_URL = 'https://chesschamp.onrender.com';
 } else {
     SERVER_URL = '';
     CLIENT_URL = '';
