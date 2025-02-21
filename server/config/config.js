4abb615c25792199c4e140d8a7a4374a6edbeeeb
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-02-21 11:06:01 +0530

    Resolved merge conflicts

--- server/config/config.js
+++ server/config/config.js
@@ -4,8 +4,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     SERVER_URL = 'https://chesschamp-backend.onrender.com';
     CLIENT_URL = 'https://chesschamp.onrender.com';
 } else {
-    SERVER_URL = 'https://chesschamp-backend.onrender.com';
-    CLIENT_URL = 'https://chesschamp.onrender.com';
+    SERVER_URL = 'http://localhost:5000';
+    CLIENT_URL = 'http://localhost:3000';
 }
 
 module.exports = { SERVER_URL, CLIENT_URL };
